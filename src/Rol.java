48a8b221ec904f9131d67d41abf398614ac2e402
ramon gamboa <ramonramon.gamboa@estudiantes.utec.edu.uy>
2025-05-26 14:18:47 -0300

    en este commit agregue la clase rol

--- src/Rol.java
+++ src/Rol.java
@@ -1,2 +1,40 @@
+import java.util.List;
+
 public class Rol {
+    private String nombre;
+    private List<String> permisos;
+
+    public Rol(String nombre, List<String> permisos) {
+        this.nombre = nombre;
+        this.permisos = permisos;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public List<String> getPermisos() {
+        return permisos;
+    }
+
+    public void setPermisos(List<String> permisos) {
+        this.permisos = permisos;
+    }
+
+    public boolean tienePermiso(String permiso) {
+        return permisos.contains(permiso);
+    }
+
+    @Override
+    public String toString() {
+        return "Rol{" +
+                "nombre='" + nombre + '\'' +
+                ", permisos=" + permisos +
+                '}';
+    }
 }
+
